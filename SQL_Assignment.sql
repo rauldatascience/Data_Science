592f6f5d1d9e5ffe4008c31e385617f24e74067e
rauldatascience <mulianaraul@gmail.com>
2020-06-11 12:48:25 +0700

    Update Assignment

---
+++ SQL_Assignment.sql
@@ -0,0 +1,301 @@
+CREATE DATABASE	ORG;
+SHOW DATABASES;
+USE ORG;
+
+CREATE TABLE Worker (
+	WORKER_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	FIRST_NAME CHAR(25),
+	LAST_NAME CHAR(25),
+	SALARY INT(15),
+	JOINING_DATE DATETIME,
+	DEPARTMENT CHAR(25)
+);
+
+INSERT INTO Worker 
+	(WORKER_ID, FIRST_NAME, LAST_NAME, SALARY, JOINING_DATE, DEPARTMENT) VALUES
+		(001, 'Monika', 'Arora', 100000, '14-02-20 09.00.00', 'HR'),
+		(002, 'Niharika', 'Verma', 80000, '14-06-11 09.00.00', 'Admin'),
+		(003, 'Vishal', 'Singhal', 300000, '14-02-20 09.00.00', 'HR'),
+		(004, 'Amitabh', 'Singh', 500000, '14-02-20 09.00.00', 'Admin'),
+		(005, 'Vivek', 'Bhati', 500000, '14-06-11 09.00.00', 'Admin'),
+		(006, 'Vipul', 'Diwan', 200000, '14-06-11 09.00.00', 'Account'),
+		(007, 'Satish', 'Kumar', 75000, '14-01-20 09.00.00', 'Account'),
+		(008, 'Geetika', 'Chauhan', 90000, '14-04-11 09.00.00', 'Admin');
+
+CREATE TABLE Bonus (
+	WORKER_REF_ID INT,
+	BONUS_AMOUNT INT(10),
+	BONUS_DATE DATETIME,
+	FOREIGN KEY (WORKER_REF_ID)
+		REFERENCES Worker(WORKER_ID)
+        ON DELETE CASCADE
+);
+
+INSERT INTO Bonus 
+	(WORKER_REF_ID, BONUS_AMOUNT, BONUS_DATE) VALUES
+		(001, 5000, '16-02-20'),
+		(002, 3000, '16-06-11'),
+		(003, 4000, '16-02-20'),
+		(001, 4500, '16-02-20'),
+		(002, 3500, '16-06-11');
+
+
+
+
+CREATE TABLE Title (
+	WORKER_REF_ID INT,
+	WORKER_TITLE CHAR(25),
+	AFFECTED_FROM DATETIME,
+	FOREIGN KEY (WORKER_REF_ID)
+		REFERENCES Worker(WORKER_ID)
+        ON DELETE CASCADE
+);
+
+INSERT INTO Title 
+	(WORKER_REF_ID, WORKER_TITLE, AFFECTED_FROM) VALUES
+ (001, 'Manager', '2016-02-20 00:00:00'),
+ (002, 'Executive', '2016-06-11 00:00:00'),
+ (008, 'Executive', '2016-06-11 00:00:00'),
+ (005, 'Manager', '2016-06-11 00:00:00'),
+ (004, 'Asst. Manager', '2016-06-11 00:00:00'),
+ (007, 'Executive', '2016-06-11 00:00:00'),
+ (006, 'Lead', '2016-06-11 00:00:00'),
+ (003, 'Lead', '2016-06-11 00:00:00');
+ 
+ 
+ /*Questions*/
+
+-- Q-1. Write an SQL query to fetch “FIRST_NAME” from Worker table using the alias name as <WORKER_NAME>. SOLVE
+
+SELECT 
+    FIRST_NAME AS first_name_worker
+FROM
+    Worker;
+
+-- Q-2. Write an SQL query to fetch “FIRST_NAME” from Worker table in upper case. SOLVE
+
+SELECT 
+    UPPER(FIRST_NAME) AS first_name_worker
+FROM
+    Worker;
+
+-- Q-3. Write an SQL query to fetch unique values of DEPARTMENT from Worker table. SOLVE
+
+SELECT DISTINCT
+    (DEPARTMENT)
+FROM
+    Worker;
+
+-- Q-4. Write an SQL query to print the first three characters of  FIRST_NAME from Worker table. SOLVE
+
+SELECT 
+    SUBSTRING(FIRST_NAME, 1, 3)
+FROM
+    Worker;
+
+-- Q-5. Write an SQL query to find the position of the alphabet (‘a’) in the first name column ‘Amitabh’ from Worker table.
+
+SELECT 
+    INSTR(FIRST_NAME, BINARY 'a')
+FROM
+    Worker
+WHERE
+    FIRST_NAME = 'Amitabh';
+
+
+-- Q-6. Write an SQL query to print the FIRST_NAME from Worker table after removing white spaces from the right side.
+
+SELECT 
+    RTRIM(FIRST_NAME)
+FROM
+    Worker;
+
+
+-- Q-7. Write an SQL query that fetches the unique values of DEPARTMENT from Worker table and prints its length.
+
+
+SELECT DISTINCT
+    LENGTH(DEPARTMENT)
+FROM
+    Worker;
+
+
+--  Q-8. Write an SQL query to print the FIRST_NAME from Worker table after replacing ‘a’ with ‘A’.
+
+
+SELECT 
+    REPLACE(FIRST_NAME, 'a', 'A')
+FROM
+    Worker;
+
+
+-- Q-9. Write an SQL query to print the FIRST_NAME and LAST_NAME from Worker table into a single column COMPLETE_NAME. A space char should separate them.
+
+SELECT 
+    CONCAT(FIRST_NAME, ' ', LAST_NAME) AS 'COMPLETE_NAME'
+FROM
+    Worker;
+
+-- Q-10. Write an SQL query to print all Worker details from the Worker table order by FIRST_NAME Ascending.
+
+SELECT 
+    *
+FROM
+    Worker
+ORDER BY FIRST_NAME ASC;
+
+
+-- Q-11. Write an SQL query to print all Worker details from the Worker table order by FIRST_NAME Ascending and DEPARTMENT Descending.
+
+
+SELECT 
+    *
+FROM
+    Worker
+ORDER BY FIRST_NAME ASC , DEPARTMENT DESC;
+
+-- Q-12. Write an SQL query to print details for Workers with the first name as “Vipul” and “Satish” from Worker table.
+
+SELECT 
+    *
+FROM
+    Worker
+WHERE
+    FIRST_NAME IN ('Vipul' , 'Satish');
+
+
+-- Q-13. Write an SQL query to print details of Workers with DEPARTMENT name as “Admin”.
+
+SELECT 
+    *
+FROM
+    Worker
+WHERE
+    DEPARTMENT LIKE 'Admin';
+
+-- Q-14. Write an SQL query to print details of workers excluding first names, “Vipul” and “Satish” from Worker table.
+
+
+SELECT 
+    *
+FROM
+    Worker
+WHERE
+    FIRST_NAME NOT IN ('Vipul' , 'Satish');
+
+
+-- Q-15. Write an SQL query to print details of Workers with DEPARTMENT name as “Admin”.
+
+SELECT 
+    *
+FROM
+    Worker
+WHERE
+    DEPARTMENT LIKE 'Admin';
+
+
+-- Q-16. Write an SQL query to print details of the Workers whose FIRST_NAME contains ‘a’.
+
+SELECT 
+    *
+FROM
+    Worker
+WHERE
+    FIRST_NAME LIKE '%a%';
+
+-- Q-17. Write an SQL query to print details of the Workers whose SALARY lies between 100000 and 500000.
+
+SELECT 
+    *
+FROM
+    Worker
+WHERE
+    SALARY BETWEEN 100000 AND 500000;
+
+
+-- Q-18. Write an SQL query to print details of the Workers who have joined in Feb’2014.
+
+SELECT 
+    *
+FROM
+    Worker
+WHERE
+    YEAR(JOINING_DATE) = 2014
+        AND MONTH(JOINING_DATE) = 2;
+
+
+-- Q-19. Write an SQL query to fetch worker names with salaries >= 50000 and <= 100000.
+
+SELECT 
+    CONCAT(FIRST_NAME, ' ', LAST_NAME) AS Worker_Name, Salary
+FROM
+    worker
+WHERE
+    WORKER_ID IN (SELECT 
+            WORKER_ID
+        FROM
+            worker
+        WHERE
+            Salary BETWEEN 50000 AND 100000);
+
+-- Q-20. Write an SQL query to print details of the Workers who are also Managers.
+
+SELECT DISTINCT
+    W.FIRST_NAME, T.WORKER_TITLE
+FROM
+    Worker W
+        INNER JOIN
+    Title T ON W.WORKER_ID = T.WORKER_REF_ID
+        AND T.WORKER_TITLE IN ('Manager');
+
+
+-- Q-21. Write an SQL query to fetch duplicate records having matching data in some fields of a table.
+
+SELECT 
+    WORKER_TITLE, AFFECTED_FROM, COUNT(*)
+FROM
+    Title
+GROUP BY WORKER_TITLE , AFFECTED_FROM
+HAVING COUNT(*) > 1;
+
+
+
+-- Q-22. Write an SQL query to clone a new table from another table.
+
+
+SELECT 
+    *
+FROM
+    Worker
+WHERE
+    MOD(WORKER_ID, 2) <> 0;
+
+-- Q-23. Write an SQL query to show the top n (say 10) records of a table.
+
+SELECT 
+    *
+FROM
+    Worker
+ORDER BY Salary DESC
+LIMIT 10;
+
+-- Q-24. Write an SQL query to determine the nth (say n=5) highest salary from a table.
+
+SELECT 
+    *
+FROM
+    Worker
+ORDER BY Salary DESC
+LIMIT 5;
+
+-- Q-25. Write an SQL query to fetch the names of workers who earn the highest salary.
+ 
+SELECT 
+    FIRST_NAME, SALARY
+FROM
+    Worker
+WHERE
+    SALARY = (SELECT 
+            MAX(SALARY)
+        FROM
+            Worker);
